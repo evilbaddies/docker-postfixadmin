f832e78a1fedeff4ca0c97e97e8b4c3df96004ce
erron <erronsilver@gmail.com>
2018-03-04 16:01:56 +0200

    Prevent passwords from being printed to log file and added an env variable for SERVER_NAME

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,15 @@ POSTFIXADMIN_DB_TYPE=mysqli
 POSTFIXADMIN_DB_HOST=$POSTFIX_DB_HOST
 POSTFIXADMIN_DB_NAME=$POSTFIX_DB_NAME
 POSTFIXADMIN_DB_USER=$POSTFIX_DB_USER
+
+set +x -v
 POSTFIXADMIN_DB_PASSWORD=$POSTFIX_DB_PASSWORD
+set -x +v
 
 if [ ! -e config.local.php ]; then
     touch config.local.php
     echo "Write config to $PWD/config.local.php"
+    set +x -v
     echo "<?php
     \$CONF['database_type'] = '${POSTFIXADMIN_DB_TYPE}';
     \$CONF['database_host'] = '${POSTFIXADMIN_DB_HOST}';
@@ -19,7 +23,8 @@ if [ ! -e config.local.php ]; then
     \$CONF['database_name'] = '${POSTFIXADMIN_DB_NAME}';
     \$CONF['setup_password'] = '${POSTFIXADMIN_SETUP_PASSWORD}';
     \$CONF['configured'] = true;
-    ?>" | tee config.local.php
+    ?>" > config.local.php
+    set -x +v
 else
     echo "WARNING: $PWD/config.local.php already exists."
     echo "Postfixadmin related environment variables have been ignored."
@@ -30,6 +35,10 @@ php create-postfixadmin-db.php
 if [ -f upgrade.php ]; then
     echo " ** Running database / environment upgrade.php "
     php upgrade.php
+fi
+
+if ! grep -q "ServerName $SERVER_NAME" /etc/apache2/apache2.conf; then
+    echo ServerName $SERVER_NAME >> /etc/apache2/apache2.conf
 fi
 
 exec "$@"
